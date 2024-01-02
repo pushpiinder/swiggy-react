48af73ffc552631ce9fae20dd45e3e057c9258d7
pushpiinder <pushpinder1997.ps@gmail.com>
2024-01-03 02:02:48 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,25 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(heading);
+root.render(heading);
+
+
+/*
+    <div id="parent">
+        <div id="child">
+            <h1>I'm h1 tag</h1>
+            <h2>I'm h2 tag</h2>
+        </div>
+    </div>
+
+    ReactElement(Object) => HTML(Browser Understands)
+*/
+const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},
+        [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]
+    )
+);
+
+console.log(parent);
+root.render(parent);
