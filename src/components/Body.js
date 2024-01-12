0ab2e56498954cfb5345f997e97dff2c7eda61a3
pushpiinder <pushpinder1997.ps@gmail.com>
2024-01-13 00:14:15 +0530

    ep-09
    all episode 09 changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 // import resList from "../utils/mockData";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
     // Local State Variable - Super powerful variable
@@ -28,6 +29,16 @@ const Body = () => {
         setListOfRestraunt(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
+
+    const onlineStatus = useOnlineStatus();
+
+    if (onlineStatus === false) {
+      return (
+        <h1>
+          Looks like you're offline!! Please check your internet connection;
+        </h1>
+      );
+    }
 
     //implemented Shimmer UI(shown on first render) - Conditional Rendering
     // if(listOfRestaurants.length === 0) {
