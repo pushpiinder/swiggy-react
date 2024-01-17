f4fac6bb49d0a5de105f10fe25c23eedb6f30a10
pushpiinder <pushpinder1997.ps@gmail.com>
2024-01-18 02:03:38 +0530

    ep-10
    All episode 10 changes. All changes are related to Tailwind classes and installation.

--- src/components/Body.js
+++ src/components/Body.js
@@ -48,12 +48,12 @@ const Body = () => {
 
     return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">  
-            <div className="filter">
-                <div className="search">
-                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+            <div className="filter flex">
+                <div className="search m-4 p-4">
+                    <input type="text" className="border border-black" value={searchText} onChange={(e) => {
                         setSearchText(e.target.value);
                     }} />
-                    <button onClick={() => {
+                    <button className="px-4 py-2 bg-green-100 m-4 rounded-lg" onClick={() => {
                         // Filter the restraunt cards and update the UI
                         // searchText
                         console.log(searchText);
@@ -65,14 +65,16 @@ const Body = () => {
                         setFilteredRestaurant(filteredRestaurant);
                     }}>Search</button>
                 </div>
-                <button className="filter-btn" onClick={() => {
-                    const filteredList = listOfRestaurants.filter(
-                    (res) => res.info.avgRating > 4
-                    );
-                    setListOfRestraunt(filteredList);
-                }}>Top Rated Restaurants</button>
+                <div className="search m-4 p-4 flex items-center">
+                    <button className="px-4 py-2 bg-gray-100 rounded-lg" onClick={() => {
+                        const filteredList = listOfRestaurants.filter(
+                        (res) => res.info.avgRating > 4
+                        );
+                        setListOfRestraunt(filteredList);
+                    }}>Top Rated Restaurants</button>
+                </div>
             </div> 
-            <div className="res-container">
+            <div className="flex flex-wrap">
             {filteredRestaurant.map((restaurant) => (
                     <Link key={restaurant.info.id} to={"/restaurants/" + restaurant.info.id}>
                         <RestaurantCard resData={restaurant} />
